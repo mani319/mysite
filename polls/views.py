b71eca5d9c3d3a452c4a4ff4248ef8d21461b58e
Manikanta T <manikanta.t@myntra.com>
2018-01-06 23:11:11 +0530

    Basic model and view for polls app

---
+++ polls/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
